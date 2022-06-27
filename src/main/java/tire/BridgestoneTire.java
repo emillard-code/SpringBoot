86af5cb218d02226b511564d67d214337ad162ff
Emerson Millard <emillard.code@gmail.com>
2022-06-27 10:59:26 -0700

    Minor changes to annotations.

--- src/main/java/tire/BridgestoneTire.java
+++ src/main/java/tire/BridgestoneTire.java
@@ -1,17 +1,19 @@
 package tire;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("bridgestone")
 public class BridgestoneTire implements Tire {
 
+    @Value("Bridgestone Driveguard Plus")
     private String name;
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
     public void brandName() {
-        System.out.println("Bridgestone");
+        System.out.println(name);
     }
 
 }
--- src/main/java/tire/FirestoneTire.java
+++ src/main/java/tire/FirestoneTire.java
@@ -1,17 +1,19 @@
 package tire;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("firestone")
 public class FirestoneTire implements Tire {
 
+    @Value("Firestone Winterforce CV")
     private String name;
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
     public void brandName() {
-        System.out.println("Firestone");
+        System.out.println(name);
     }
 
 }
